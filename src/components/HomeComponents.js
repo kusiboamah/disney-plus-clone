7688858ffb6c44b70b57061279401f09a2ffaebb
kusi boamah <rangeroverk7@gmail.com>
2022-02-18 10:27:49 +0000

    ADD: Header And images

---
+++ src/components/HomeComponents.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function HomeComponents() {
+  return (
+    <Container>
+        HomeComponents
+    </Container>
+  )
+}
+
+export default HomeComponents
+
+const Container = styled.div`
+    
+`
